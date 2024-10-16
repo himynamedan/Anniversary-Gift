9b0aaf4597aaebeea5823e09224c96e9d7635936
Danish <iamdanish155@gmail.com>
2024-10-16 14:20:21 +0500

    Updated CSS styling

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';  // Importing CSS for Navbar
+import logo from '../Assets/ushdan.png';  // Import the logo
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      {/* Logo section */}
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
       <ul className="navbar-list">
         <li><Link to="/" className="nav-link">History</Link></li>
         <li><Link to="/messages" className="nav-link">Our Texts</Link></li>
