57c9c3bcf3bd5e75b2847cccff566650e9d49556
Danish <iamdanish155@gmail.com>
2024-11-03 18:03:40 +0500

    Anniversay page update

--- src/Components/anniversary.js
+++ src/Components/anniversary.js
@@ -5,10 +5,71 @@ const AnniversaryPage = () => {
   return (
     <div className="anniversary-container">
       <h1>Happy 3rd Anniversary, My Love! 💖</h1>
+      <p>My girl</p>
+      <p>My pretty girl</p>
+      <p>My love</p>
+      <p>My only love</p>
+      <p>My baby</p>
+      <p>My baby girl</p>
+      <p>My princess</p>
+      <p>My beautiful princess</p>
+      <p>My sunshine</p>
+      <p>My morning sunshine</p>
+      <p>My moon</p>
+      <p>My midnight moon</p>
+      <p>My life</p>
+      <p>My whole life</p>
+      <h4>Meri jaan</h4>
+      <h3>Meri pyaari jaan</h3>
+      <h2>HAPPY ANNIVERSARY USHNA JEEEEEEEE</h2>
       <p>
-        It’s been an incredible journey with you by my side, and I feel so lucky to have you. 
-        Every moment we've shared has been a blessing, and I can’t wait to create even more memories with you. 
-        Here’s to us and the beautiful love we’ve nurtured over the last 3 years. I love you more with every passing day. 🌸💐
+      Do you realise how wonderful it is that we have spent 3 years together<br></br>
+      It's been 3 years since I have been falling for those eyes everyday<br></br>
+      3 years since that lovely smile has been making my day<br></br>
+      We've gone through a lot<br></br>
+      A lot more than anyone can imagine<br></br>
+      Or survive<br></br>
+      We've been each others right arm<br></br>
+      Helping and supporting at every point of our lives<br></br>
+      I am the luckiest man alive<br></br>
+      Because I have<br></br>
+      I swear to GOD<br></br>
+      I could not as for anything more<br></br>
+      You are my everything<br></br>
+      And I truly mean it when I say that<br></br>
+      You are the love of my life<br></br>
+      You are a breath of fresh breeze for me<br></br>
+      You're perfect in every sense for me<br></br>
+      A blessing in disguise<br></br>
+      A dream come true<br></br>
+      A visit to a cocomo factory<br></br>
+      A wall full of salanty stickers<br></br>
+      A 100 hour album of Eminem songs<br></br>
+      A 7 course meal of beef burgers<br></br>
+      A track ride of my favorite car koenigsegg<br></br>
+      You're all of them to me<br></br>
+      Your voice sounds like a red switch mechanical keyboard to me<br></br>
+      So soothing that I could fall asleep in your lap<br></br>
+      And have dreams of us doing the most romantic things<br></br>
+      I love you<br></br>
+      I really really love you<br></br>
+      With all of my heart<br></br>
+      My heart belongs to you<br></br>
+      And it will be yours forever<br></br>
+      I promise<br></br>
+      Nobody in this life or the other could ever take your place<br></br>
+      You're the most precious thing to me in all the universe<br></br>
+      It's been only 3 years<br></br>
+      We have a whole lifetime together<br></br>
+      We will go to the prettiest places<br></br>
+      Have the best laughs<br></br>
+      Eat our favourite foods<br></br>
+      All of this together<br></br>
+      Cuz I am falling for you more every single day my love<br></br>
+      Happy anniversary my baby girl<br></br>
+      I love you<br></br>
+      I LOVE YOU SOOOOOOOOOOO MUCHHHHHHHHH<br></br>
+. 🌸💐
       </p>
       <div className="heart-container">
         {/* Add flowers and hearts */}
